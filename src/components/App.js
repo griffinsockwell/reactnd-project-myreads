3a508258510c8a087153011af3225c303e8beacc
griffinsockwell <gs71312@icloud.com>
2017-08-03 16:27:37 -0400

    Remove bootstrapped code

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledApp = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const App = () => <StyledApp>App</StyledApp>;
+
+export default App;
